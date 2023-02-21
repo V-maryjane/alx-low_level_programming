72014d0c25e4fc4cd9fea70558eea3b5d27ef6bc
Maryjane <mejiabot241@gmail.com>
2023-02-21 01:10:33 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/*
+* -putchar write the character in A to stdout.
+*  A the character to print.
+*  Return on success 1.
+*  on error -1 is return.
+*/
+int -putchar(char);
+{
+	return (write (1,&A, i));
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+In this project, we will see how theuse functions, nested loop in C.
