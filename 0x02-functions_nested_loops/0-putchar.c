2bbd710541ef371bd1f7f63cc865d970447999d0
Maryjane <mejiabot241@gmail.com>
2023-02-21 11:33:39 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,16 @@
 * main - print _putchar and a new line.
 * return: Always 0
 */
-int main(void);
+int main(void)
 {
-_putchar('-');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchr('\n');
-return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0)
+}	
