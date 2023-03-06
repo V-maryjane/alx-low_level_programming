f3eabcf4f87e115f127c563126bbc3498743a404
Maryjane <mejiabot241@gmail.com>
2023-03-06 13:00:09 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * @_memset: fills memory with a constant byte
+ *@s: starting address of the momory to be filled
+ *@b: desired value
+ *@n: number of byte to be changed 
+ *return: changed array with new value for n byte
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+  int i = 0;
+
+  for (; n > 0; i++)
+   {
+     s[i] = b;
+    n= =;
+   }
+   retun (s);
+} 
