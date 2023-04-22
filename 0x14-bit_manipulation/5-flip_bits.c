b0e052720bd114e30bac092da76b5737b6f09870
Maryjane <mejiabot241@gmail.com>
2023-04-22 01:57:19 -0700

    flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
  * flip_bits - number of bits
  * @n: first num
@@ -7,17 +10,17 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int b = 0;
-	for (n != 0 || m != 0)
+unsigned int b = 0;
+for (n != 0 || m != 0)
 
 {
-		if ((n & 1)  != (m & 1))
-			b++;
-		n = n >> 1;
-		m = m >> 1;
+if ((n & 1)  != (m & 1))
+b++;
+n = n >> 1;
+m = m >> 1;
 	}
-	return (b);
+return (b);
 }
 
-	
+
 }
