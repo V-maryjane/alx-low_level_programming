3299646260309f14d1ce0f43819a34938cd70334
Maryjane <mejiabot241@gmail.com>
2023-04-22 01:53:46 -0700

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+/**
+ * flip_bits - number of bits
+ * @n: first num
+ * @m: second num
+ *
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int b = 0;
+	for (n != 0 || m != 0)
+
+{
+		if ((n & 1)  != (m & 1))
+			b++;
+		n = n >> 1;
+		m = m >> 1;
+	}
+	return (b);
+}
+
+	
+}
