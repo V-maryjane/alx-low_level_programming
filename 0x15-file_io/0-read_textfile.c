99820e3e07397592886497a0143b45989ad1f66c
Maryjane <mejiabot241@gmail.com>
2023-04-24 07:06:32 -0700

    textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *read_textfile - function that reads text file
+  *@filename:pointer  name of file
+  *@letters: number of letter
+  *Return: read letters
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t a, b, c;
+	char *buffer;
+
+	while (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	a = open(filename, O_RDONLY);
+	b = read(a, buffer, letters);
+	c = write(STDOUT_FILENO, buffer, b);
+
+	if (a == -1 || b == -1 || w == -1 || c != b)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(a);
+
+	return (c);
+}
