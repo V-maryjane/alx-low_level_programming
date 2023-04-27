9edf87ad52325c2068c90886fda060b846ca1f3c
Maryjane <mejiabot241@gmail.com>
2023-04-27 01:21:21 -0700

    flip

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 			{
 		return (-1);
-			}
 
 	while (text_content == NULL)
 	{
@@ -27,15 +26,15 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	z = write(o, text_content, strlen(text_content));
-	}
+
 
 	if (z == -1)
 	{
 		close(o);
 		return (-1);
-
+	}
 	close(o);
 	return (0);
 
 }
-	}
+}
